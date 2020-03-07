c1269b71d72b2f478bf97d5b99162dcc18fa9f28
kszosze <kszosze@gmail.com>
2020-03-07 14:08:26 +0100

    Fix gradle build

--- src/main/java/com/mace/runner/Application.java
+++ src/main/java/com/mace/runner/Application.java
@@ -1,22 +1,7 @@
 package com.mace.runner;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.TimeUnit;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-
-import com.mace.runner.model.Head;
-import com.mace.runner.model.Message;
-import com.mace.runner.model.Runner;
 
 @SpringBootApplication
 public class Application {
